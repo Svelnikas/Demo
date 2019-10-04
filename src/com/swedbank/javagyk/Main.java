977f56454f19cf2c92d0ee9dade5629c5109deb8
Antanas Svelnikas <antanassvelnikas@Antanass-MacBook-Air.local>
2019-10-04 10:56:21 +0300

    Initial version

---
+++ src/com/swedbank/javagyk/Main.java
@@ -0,0 +1,9 @@
+package com.swedbank.javagyk;
+
+import sun.jvm.hotspot.memory.CardGeneration;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
